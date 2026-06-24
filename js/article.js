38591250e83f6cc595ec5a39ab0bbfafc95483cb
Kamran Muradov <muradkamran35@gmail.com>
2026-06-24 21:03:50 +0400

    Update article.js

--- js/article.js
+++ js/article.js
@@ -3,7 +3,7 @@ let db = { articles: [] };
 
 const api = {
     getAll: async () => {
-        const response = await fetch('json/journews_db.articles.json');
+        const response = await fetch('json/journews_db.articles.json?t=' + new Date().getTime());
         if (!response.ok) throw new Error('Network error');
         return await response.json();
     }
