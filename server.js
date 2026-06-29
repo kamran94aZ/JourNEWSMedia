4e14165b30f3928861a45c4a44b6352e760d9600
Kamran Muradov <muradkamran35@gmail.com>
2026-06-29 19:42:25 +0400

    Update server.js to improve API and static file serving

--- server.js
+++ server.js
@@ -55,4 +55,4 @@ app.get('/api/:category', (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
