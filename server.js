eaf7fd827fac0b48dfd0dcd38d52a05e3b226789
Kamran Muradov <muradkamran35@gmail.com>
2026-07-02 13:25:26 +0400

    Change server port from 3000 to 8080

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 const API_KEY = process.env.NEWS_API_KEY;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // CORS konfiqurasiyası
 app.use(cors({
